3a3ac095718fb2cc33793425985f0f5887009668
Ingrid Elias <elias.ingrid@yahoo.com>
2015-12-12 15:03:31 +0900

    Checkpoint 6 completed, Services Part 1

---
+++ app/scripts/services/Fixtures.js
@@ -0,0 +1,47 @@
+(function() {
+    function Fixtures() {
+        var Fixtures = {};
+        
+        var albumPicasso = {
+            name: 'The Colors',
+            artist: 'Pablo Picasso',
+            label: 'Cubism',
+            year: '1881',
+            albumArtUrl: '/assets/images/album_covers/01.png',
+            songs: [
+                { name: 'Blue', length: 161.71, audioUrl: '/assets/music/bloc_jams_music/blue'},
+                { name: 'Green', length: 103.71, audioUrl: '/assets/music/bloc_jams_music/green'},
+                { name: 'Red', length: 268.45, audioUrl: '/assets/music/bloc_jams_music/red'},
+                { name: 'Pink', length: 153.14, audioUrl: '/assets/music/bloc_jams_music/pink'},
+                { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/bloc_jams_music/magenta'}
+            ]
+        };
+
+
+        var albumMarconi = {
+          name: 'The Telephone',
+            artist: 'Guglielmo Marconi',
+            label: 'EM',
+            year: '1909',
+            albumArtUrl: '/assets/images/album_covers/20.png',
+            songs: [
+                { name: 'Hello, Operator?', length: '1:01' },
+                { name: 'Ring, ring, ring', lenght: '5:01' },
+                { name: 'Fits in your pocket', length: '3:21' },
+                { name: 'Can you hear me now?', lenght: '3:14' },
+                { name: 'Wrong phone number', length: '2:15' }
+            ]
+        };
+        
+        Fixtures.getAlbum = function() {
+          return albumPicasso;  
+        };
+        
+        
+        return Fixtures;
+    }
+    
+    angular
+        .module('blocJams')
+        .factory('Fixtures', Fixtures);
+})();
