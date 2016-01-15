f1de1c510eab9d6c887f95bcdf001be569889ee3
Ingrid Elias <elias.ingrid@yahoo.com>
2016-01-15 11:26:39 +0900

    Checkpoint 10 finished, directives part 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function (){
-    function SongPlayer(Fixtures){
+    function SongPlayer($rootScope, Fixtures){
         var SongPlayer = {};
          
         /**
@@ -31,6 +31,12 @@
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
+            });
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
             });
 
             SongPlayer.currentSong = song;
@@ -67,6 +73,13 @@
         */
         
         SongPlayer.currentSong = null;
+        
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        
+        SongPlayer.currentTime = null;
         
         SongPlayer.play = function(song) {
              song = song || SongPlayer.currentSong;
@@ -105,6 +118,18 @@
         };
         
         /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        
+        SongPlayer.setCurrentTime = function(time) {
+            if(currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+        
+        /**
         * @function previous
         * @desc Changes the currentSong to the previous song by decrementing the song index
         */
@@ -128,5 +153,5 @@
     
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
